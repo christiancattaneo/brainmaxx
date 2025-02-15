a6d38735cab40c2274cb248b6c3e160232b62bea
Christian Cattaneo <christian.cattaneo@gauntletai.com>
2025-02-15 14:59:52 -0600

    Initial Commit

---
+++ BrainmaxxTests/BrainmaxxTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrainmaxxTests.swift
+//  BrainmaxxTests
+//
+//  Created by Christian Cattaneo on 2/15/25.
+//
+
+import Testing
+@testable import Brainmaxx
+
+struct BrainmaxxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
